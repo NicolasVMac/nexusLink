5f6cb859f518a3580708b7913a6f0f9d272ff65d
CAS <alejo.hortua3@gmail.com>
2025-08-11 17:56:02 -0500

    Ajuste

--- views/modules/encuestas/inicio.php
+++ views/modules/encuestas/inicio.php
@@ -1,7 +1,7 @@
 <div class="content">
 
     <h1 class="mb-2">Modulo Auditoria Inicio</h1>
-    <div class="row">
+    <!-- <div class="row">
         <div class="col-sm-12 col-md-4">
             <div class="card border border-300 h-100 w-100 overflow-hidden">
                 <div class="card-body px-5 position-relative">
@@ -13,7 +13,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 </div>
 
 <script src="views/js/encuestas/inicio.js?v=<?= md5_file('views/js/encuestas/inicio.js') ?>"></script>
